e6f14801e2863d91aafdaec951f3887f5749bd37
Yerassyl <es2010218@gmail.com>
2023-12-24 13:59:17 +0600

    added classes point and shape

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import models.Point;
+import models.Shape;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+            File file = new File("C:\\Users\\es201\\IdeaProjects\\assignment1se2311\\src\\source");
+
+        Scanner sc = new Scanner(file);
+        Shape shape = new Shape();
+
+        while (sc.hasNext()) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+
+            Point point = new Point(x, y);
+            shape.addPoint(point);
+        }
+
+        System.out.println(shape.getPerimeter());
+        System.out.println(shape.getLongest());
+    }
+}
